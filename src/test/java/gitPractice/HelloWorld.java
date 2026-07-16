08bad2e8d351ff7b39c23d4ed57b848ef7d2d353
BhabeshParida <paridabhabesh2018@gmail.com>
2026-07-16 12:55:10 +0530

    git message updated

--- src/test/java/gitPractice/HelloWorld.java
+++ src/test/java/gitPractice/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("HI BHABESH");
+		System.out.println("hello git");
 
 	}
 
